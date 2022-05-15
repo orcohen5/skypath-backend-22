5097a81fa46f26149692d54f5e0b2ddc6e02f2da
jacksnett <yaakovsnett1@gmail.com>
2022-05-15 11:42:15 +0300

    initialized project

---
+++ src/main/kotlin/skypath/skypathbackend22/SkypathBackend22Application.kt
@@ -0,0 +1,11 @@
+package skypath.skypathbackend22
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SkypathBackend22Application
+
+fun main(args: Array<String>) {
+	runApplication<SkypathBackend22Application>(*args)
+}
---
+++ src/test/kotlin/skypath/skypathbackend22/SkypathBackend22ApplicationTests.kt
@@ -0,0 +1,13 @@
+package skypath.skypathbackend22
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SkypathBackend22ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
